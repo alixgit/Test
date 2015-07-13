8ab7593c5dadbf45d322cc3c7270292c8d1f1c9f
Alix Ndembi <alix.ndembi@senseisrl.it>
2015-07-13 16:50:44 +0200

    secondo commit

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Questa e' una piccola prova");
+		System.out.println("Questa e' un piccolo test");
 
 	}
 
