dc71f2d102f43a1119f82430f5b9a80d12547620
Alix Ndembi <alix.ndembi@senseisrl.it>
2015-07-14 11:58:23 +0200

    Commit gioco

--- src/Test.java
+++ src/Test.java
@@ -1,16 +1,16 @@
 
 public class Test {
 	
-	public static int add(int a, int b) {
+	public static int meno(int a, int b) {
 
-		return a + b;
+		return a - b;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Questa e' un piccolo test");
 		System.out.println("deuxième modification");
-		System.out.println("Il risultato della nostra somma è: " + add(10, 9));
+		System.out.println("Il risultato della nostra somma è: " + meno(10, 9));
 		System.out.println("metodo semplificato");
 		
 
