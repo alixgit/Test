56ca5dc54ee15565725c3a19e82cf1610a1ea1b6
Alix Ndembi <alix.ndembi@senseisrl.it>
2015-07-14 09:44:45 +0200

    aggiunti due variabili

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,16 @@
 
 public class Test {
+	
+	public static int add(int a, int b) {
+		int result = a + b;
+		return result;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Questa e' un piccolo test");
 		System.out.println("deuxième modification");
-		int a, b;
+		System.out.println("Il risultato della nostra somma è: " + add(5, 9));
 		
 
 	}
