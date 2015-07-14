4e7c05318caaf1f331cc9bfbb30112f196f94ea6
Alix Ndembi <alix.ndembi@senseisrl.it>
2015-07-14 10:06:53 +0200

    la dernière version

--- src/Test.java
+++ src/Test.java
@@ -2,15 +2,16 @@
 public class Test {
 	
 	public static int add(int a, int b) {
-		int result = a + b;
-		return result;
+
+		return a + b;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Questa e' un piccolo test");
 		System.out.println("deuxième modification");
-		System.out.println("Il risultato della nostra somma è: " + add(5, 9));
+		System.out.println("Il risultato della nostra somma è: " + add(10, 9));
+		System.out.println("metodo semplificato");
 		
 
 	}
