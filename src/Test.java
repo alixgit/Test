9049bcf6c9c41b7e33254c6cf1000ea0845345ff
Alix Ndembi <alix.ndembi@senseisrl.it>
2015-07-14 12:22:54 +0200

    abiamo capito bene

--- src/Test.java
+++ src/Test.java
@@ -9,7 +9,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Questa e' un piccolo test");
-		System.out.println("deuxième modification");
+		System.out.println("adesso abbiamo capito il gioco");
 		System.out.println("Il risultato della nostra somma è: " + meno(10, 9));
 		
 
